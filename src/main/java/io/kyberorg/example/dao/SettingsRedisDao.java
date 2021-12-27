1c1d84ce56c79ec21de4e28e3f6d50e56318ccc1
Aleksandr Muravja <alex@kyberorg.io>
2021-12-27 17:22:55 +0200

    Adding redis to another profiles. Code fixes.
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/example/dao/SettingsRedisDao.java
+++ src/main/java/io/kyberorg/example/dao/SettingsRedisDao.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Repository;
 import javax.annotation.PostConstruct;
 import java.util.Optional;
 
+/**
+ * Dao for {@link Settings}.
+ */
 @RequiredArgsConstructor
 @Repository
 public class SettingsRedisDao extends RedisDao {
@@ -23,10 +26,21 @@ public class SettingsRedisDao extends RedisDao {
         this.hashName = getHashName(Settings.class);
     }
 
+    /**
+     * Create or update {@link  Settings}.
+     *
+     * @param settings object to store.
+     */
     public void save(final Settings settings) {
         hashOperations.put(hashName, settings.getSessionId(), settings);
     }
 
+    /**
+     * Retrieve {@link Settings} by its ID.
+     *
+     * @param sessionId string with session id used as key.
+     * @return {@link Optional} which contains {@link Settings} or not.
+     */
     public Optional<Settings> get(final String sessionId) {
         return Optional.ofNullable(hashOperations.get(hashName, sessionId));
     }
--- src/main/java/io/kyberorg/example/model/Settings.java
+++ src/main/java/io/kyberorg/example/model/Settings.java
@@ -11,6 +11,12 @@ public class Settings implements Serializable {
     private String sessionId = null;
     private boolean lightMode;
 
+    /**
+     * Creates empty Settings for given session.
+     *
+     * @param sessionId string with session id linked to settings.
+     * @return {@link Settings} with defaults.
+     */
     public static Settings createNewForSession(final String sessionId) {
         Settings settings = new Settings();
         settings.setSessionId(sessionId);
